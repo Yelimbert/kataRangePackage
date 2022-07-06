91a3700e2ae6ffb00c51d0069fc749b2b02cb346
Yelimbert <ijoselohd@gmail.com>
2022-07-06 00:32:28 -0400

    kata range

---
+++ kataRangePackage/kataRangePackage/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace kataRangePackage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(KataR.KataRange.overlaps("[3,6]", "[4, 7]"));
+            Console.WriteLine(KataR.KataRange.overlaps("[1,3]", "[4, 7]"));
+
+            Console.WriteLine(KataR.KataRange.equals("(3,7)", "(3,7)"));
+            Console.WriteLine(KataR.KataRange.equals("(3,7)", "(3,9)"));
+        }
+    }
+}
